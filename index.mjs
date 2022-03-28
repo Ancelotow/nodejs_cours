ba915650c9d90f9b61df38671a00d00f17640b64
Ancelotow <ancelotow@gmail.com>
2022-03-28 12:51:22 +0200

    Exercices function CRUD without API

--- index.mjs
+++ index.mjs
@@ -1,10 +1,41 @@
-import { add, getAll, addAsync } from "./product.mjs";
+import { getAll, addAsync, update, remove, Product } from "./product.mjs";
 
 const arr = []
-arr.push(addAsync("Coca Cola"))
-arr.push(addAsync("Fanta"))
-arr.push(addAsync("Sprite"))
-Promise.all(arr).then(() => {
+arr.push(addAsync(new Product("Coca Cola", 9)))
+arr.push(addAsync(new Product("Sprite", 10)))
+arr.push(addAsync(new Product("Fanta", 34)))
+arr.push(addAsync(new Product("Fanta", 9)))
+
+// Test ajout
+try{
+    await Promise.all(arr);
+} catch(error) {
+    console.error(error.message);
+} finally {
     getAll()
-})
+}
+
+// Test modification
+try{
+    update("Sprite", 4)
+    update("ds", 4)
+} catch (error) {
+    console.error(error.message)
+} finally {
+    getAll()
+
+}
+
+// Test suppression
+try{
+    remove("Sprite", 4)
+    remove("Fanta", 10)
+    remove("ds", 4)
+} catch (error) {
+    console.error(error.message)
+} finally {
+    getAll()
+
+}
+
 
--- product.mjs
+++ product.mjs
@@ -1,26 +1,67 @@
 const products = []
 
-const add = (product = "") => {
-    setTimeout(() => {
+class Product {
+    name = ""
+    qty = 0
+
+    constructor(_name, _qty) {
+        this.name = _name
+        this.qty = _qty
+    }
+}
+
+const add = (product = new Product()) => {
+    if(getByName(product.name)) {
+        throw new Error("Product already exists.")
+    } else {
         products.push(product)
-        console.log('Le produit ', product, ' à été ajouté.')
-    }, 1000)
+        console.log('Product ', product.name, ' has been added !')
+    }
 };
 
-const addAsync = async (product = "") => {
-    return new Promise((resolve, _) => {
+const addAsync = (product = new Product()) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            products.push(product)
-            console.log('Le produit ', product, ' à été ajouté.')
-            resolve()
+            try {
+                add(product)
+                resolve()
+            } catch (e) {
+                reject(e)
+            }
         }, 1000)
     });
 };
 
 const getAll = () => {
-    products.forEach(prd => console.log(prd))
+    products.forEach(prd => console.log(prd.name, ' : ', prd.qty))
 };
 
-export { add, getAll, addAsync };
+const getByName = (name = "") => {
+    return products.find(prd => prd.name === name)
+};
+
+const update = (name, qty) => {
+    let product = getByName(name);
+    if(product){
+        product.qty = qty
+    } else {
+        throw new Error("Not found product.")
+    }
+};
+
+const remove = (name, qty) => {
+    let product = getByName(name);
+    if(product){
+        product.qty -= qty
+        if(product.qty <= 0) {
+            let index = products.indexOf(product)
+            products.splice(index, 1)
+        }
+    } else {
+        throw new Error("Not found product.")
+    }
+};
+
+export { add, getAll, addAsync, getByName, update, remove, Product };
 
 
