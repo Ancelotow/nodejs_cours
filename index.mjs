554ac1de0ea5ef15c2da1e6eb579c9e1c7f247df
Ancelotow <ancelotow@gmail.com>
2022-03-30 15:04:06 +0200

    Config Heroku

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ import morgan from 'morgan'
 
 const app = express();
 
-const port = 3000
+const port = process.env.PORT || 3000
 const host = '127.0.0.1'
 
 app.use(bodyParser.urlencoded({ extended: false }))
