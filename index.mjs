682506c37c44233f8a04e32284880e9fb2afbd9b
Ancelotow <ancelotow@gmail.com>
2022-03-30 15:10:52 +0200

    Fix heroku error

--- index.mjs
+++ index.mjs
@@ -6,7 +6,7 @@ import morgan from 'morgan'
 const app = express();
 
 const port = process.env.PORT || 3000
-const host = '127.0.0.1'
+const host = process.env.HOST || '127.0.0.1'
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(morgan('dev'))
 app.use(router)
 
-app.listen(port, host, () => {
+app.listen(port, () => {
     console.log(`Server running on http://${host}:${port}`)
 })
 
